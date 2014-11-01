b183853a8ab0c8c02839243932d2152868c8d857
Virendra <chouhan.virendra@gmail.com>
2014-11-01 19:23:37 +0530

    details page changes and some navigation changes

--- app/controllers/pagesController.js
+++ app/controllers/pagesController.js
@@ -42,9 +42,43 @@ pagesController.showDetail = function(req, res){
 			var i = -1;
 			var dt = '';
 			var resData = [];		
+			var dm = 0;		
+			var citation = '';
+			var citation_issued = 0;
+			var iconurl = '';
+			var icon_color = '';
+			var imgborderclass = '';
           	function nextRecord(){
 	            i++;
 	            if(i < data.length){
+	            	dm+= parseInt(data[i].demerits);
+	            	if(citation == '' && data[i].citation_issued == 1) 
+	            	{
+	            		citation_issued = 1;
+	            		citation = '1';	            	
+	            	}
+	            	var type = '';
+	            	if(data[i].type == "Retail with food prep")
+		            {
+		              type = "retailwithfoodprep";
+		            }
+		            else if(data[i].type == "Snow cone stand")
+		            {
+		              type = "snowconestand";
+		            }
+		            else if(data[i].type == "Farmer's Market")
+		            {
+		              type = "farmersmarket";
+		            }
+		            else if(data[i].type == "Long Term/Nursing Home/Assisted Living")
+		            {
+		              type = "ltnhal";
+		            }
+		            else
+		            {
+		            	type = data[i].type.replace(" ","").toString().toLowerCase();
+		            }
+		            data[i].icontype = type;
 	            	var formattedDate = new Date(data[i].last_inspection.toString());
 					var d = formattedDate.getDate();
 					var m =  formattedDate.getMonth();
@@ -83,10 +117,56 @@ pagesController.showDetail = function(req, res){
 	            else 
 	            {
 	            	//console.log('resData',resData);
+	            	if(data.length == 1)
+	            	{
+	            		if(data[0].oo_compliance == "No Violations Found")
+	            		{
+							iconurl = "/images/violations/"+ data[0].icontype + "_green.png";
+							icon_color = '#009933';
+							imgborderclass = 'img-noviolation';
+	            		}
+	            		else if(data[0].oo_compliance == "Yes" && parseInt(data[0].demerits) > 15)
+	            		{
+	            			iconurl = "/images/violations/"+ data[0].icontype + "_red.png";
+	            			icon_color = '#990000';
+	            			imgborderclass = 'img-violation';
+	            		}
+	            		else
+	            		{
+	            			iconurl = "/images/violations/"+ data[0].icontype + "_yellow.png";
+	            			icon_color = '#cc9900';
+	            			imgborderclass = 'img-normalviolation';
+	            		}
+	            	}
+	            	else
+	            	{
+
+	            		if(parseInt(dm) > 15 || citation_issued == 1)
+			            {
+			            	iconurl = "/images/violations/"+ data[0].icontype + "_red.png";
+	            			icon_color = '#990000';
+	            			imgborderclass = 'img-violation';
+			            }
+			            else if(parseInt(dm) < 15 && citation_issued == 0)
+			            {
+				            iconurl = "/images/violations/"+ data[0].icontype + "_yellow.png";
+	            			icon_color = '#cc9900';
+	            			imgborderclass = 'img-normalviolation';
+			            }
+			            else
+			            {
+			            	iconurl = "/images/violations/"+ data[0].icontype + "_green.png";
+							icon_color = '#009933';
+							imgborderclass = 'img-noviolation';
+			            }
+	            	}	            	
 	              	th.render('pages/detail',{data:data, 
 				lastInspection:monthArray[dateObj.getMonth()] + " " + dateObj.getDate() + ", " + dateObj.getFullYear(),
 				updatedAt : monthArray[dateObj1.getMonth()] + " " + dateObj1.getDate() + ", " + dateObj1.getFullYear(),
-						inspData : resData
+						inspData : resData,
+						iconurl : iconurl,
+						iconcolor : icon_color,
+						imgborderclass : imgborderclass
 			});
 	            }
 	          }
